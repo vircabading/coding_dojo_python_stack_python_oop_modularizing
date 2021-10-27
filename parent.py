dd63212fe23f8f6756b5d7eab557a863127ba5ea
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-26 22:25:36 -0700

    created a user

--- parent.py
+++ parent.py
@@ -3,12 +3,30 @@
 # By:   Virgilio D. Cabading Jr.    Created: October 26, 2021
 # //////////////////////////////////////////////////////////////
 
+# //////////////////////////////////////////////////////////////
+# //// VARIABLES ///////////////////////////////////////////////
+
 local_val = "magical unicorns"
+
+# //// FUNCTIONS ///////////////////////////////////////////////
+
 def square(x):
     return x * x
+
+# //// CLASSES /////////////////////////////////////////////////
+
 class User:
     def __init__(self, name):
         self.name = name
+
     def say_hello(self):
         return "hello"
+
+# //// MAIN EXECUTABLE SECTION /////////////////////////////////
+
+# in the same file, add the following below the User class
+print(square(5))
+user = User("Anna")
+print(user.name)
+print(user.say_hello())
 
