66ee42df216e88b6d941749eeadcf645c072644b
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-26 22:18:33 -0700

    add comment headers

---
+++ parent.py
@@ -0,0 +1,14 @@
+# //////////////////////////////////////////////////////////////
+# Subj: Coding Dojo > Python Stack > Python > OOP: Modularizing
+# By:   Virgilio D. Cabading Jr.    Created: October 26, 2021
+# //////////////////////////////////////////////////////////////
+
+local_val = "magical unicorns"
+def square(x):
+    return x * x
+class User:
+    def __init__(self, name):
+        self.name = name
+    def say_hello(self):
+        return "hello"
+
